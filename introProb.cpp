9a3a496eca7dd3cf58ad4f595d3a533aa89ca7d6
Mohit Sharma <sharma.132@iitj.ac.in>
2025-02-16 20:22:09 +0530

    lets sync2

--- introProb.cpp
+++ introProb.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void WierdAlgo() {
@@ -61,17 +62,128 @@ void IncreasingArray() {
     cout << moves;
 }
 
+void Permutations() {
+    long long n;
+    cin >> n;
+    if (n == 1) {
+        cout << 1;
+    } else if (n < 4) {
+        cout << "NO SOLUTION";
+    } else {
+        for (int i = 2; i <= n; i += 2) {
+            cout << i << " ";
+        }
+        for (int i = 1; i <= n; i += 2) {
+            cout << i << " ";
+        }
+    }
+}
+
+void NumberSpiral() {
+    long long t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        long long y, x;
+        cin >> y >> x;
+        long long n = y > x ? y : x;
+        long long base = n * n - 2*n + 1;
+        if (n % 2 == 0) {
+            if (x == n) {
+                cout << base + y;
+            } else {
+                cout << base + n + y-x;
+            }
+        } else {
+            if (y == n) {
+                cout << base + x;
+            } else {
+                cout << base + n + x - y;
+            }
+        }
+        cout << endl;
+    }
+}
+
+void TwoKnights() {
+    long long n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        // total = no of block C 2 or nc2 where n = i * i
+        long long total = 1LL * i * i * (1LL * i * i - 1) / 2;
+
+        // attack = no of blocks where knight can attack each other
+        // can be find by given answer for 1 to 8 - total and intution for formula
+        long long attack = 1LL * 4 * (i - 1) * (i - 2);
+        cout << total - attack << endl;
+    }
+}
+
+void TwoSets(){
+    int n;
+    cin >> n;
+    if(n==1 || n==2){
+        cout << "NO" << endl;
+        return;
+    }
+    vector<int> a, b;  
+    if ((n-3)%4 == 0) {
+        cout << "YES" << endl;
+        a.push_back(1);
+        a.push_back(2);
+        b.push_back(3);
+        for(int i=4; i<=n; i++){
+            if(i%4 == 0 || i%4 == 3){
+                a.push_back(i);
+            } else {
+                b.push_back(i);
+            }
+        }
+    } else if((n-4)%4 == 0) {
+        cout << "YES" << endl;
+        for(int i=1; i<=n; i++){
+            if(i%4 == 0 || i%4 == 1){
+                a.push_back(i);
+            } else {
+                b.push_back(i);
+            }
+        }
+    } else {
+        cout << "NO" << endl;
+        return;
+    }
+    cout << a.size() << endl;
+    for(int i=0; i<a.size(); i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    cout << b.size() << endl;
+    for(int i=0; i<b.size(); i++){
+        cout << b[i] << " ";
+    }
+    cout << endl;
+}
+
+void BitStrings() {
+    long long n;
+    cin >> n;
+    long long mod = 1e9 + 7;
+    long long ans = 1;
+    for (int i = 0; i < n; i++) {
+        ans = (ans * 2) % mod;
+    }
+    cout << ans;
+}
 int main() {
-      
+    ios::sync_with_stdio(false); cin.tie(nullptr);
     // WierdAlgo();
     // MissingNumber();
     // Repetitions();
-    IncreasingArray();
+    // IncreasingArray();
     // Permutations();
     // NumberSpiral();
     // TwoKnights();
     // TwoSets();
-    // BitStrings();
+    BitStrings();
     // TrailingZeros();
     // CoinPiles();
     // PalindromeReorder();
